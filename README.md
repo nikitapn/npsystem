3ac415ba31efd33b31375ccaf6e3eb8d33172d86
Nikita Pennie <nikitapnn1@gmail.com>
2025-05-04 23:39:34 +0300

    README up

--- README.md
+++ README.md
@@ -1,13 +1,46 @@
 # npsystem
 
-I created this to control various things in my house (pumps, well, valves, automatic plant watering, etc...). The goal was to create scalable, cheap, reliable system working 24/365 without reboot, where all controllers are independent and do not require main server to run logic algorithms. The system is built on RS-485 with signal loss detection. The communication protocol is based on set of protocols utilizing virtual marker. Each controller has I2C interface for additional modules with custom software (like 1-wire devices, or any other). The bridge between RS-485 and ethernet is built on enc28j60 with atmega8, the most expensive part is max3082 transivers (2 pieces for controlles and bridge), the controllers could be either atmega8 or atmega16, in future I'm planning to add stm32 chips. \
-\
-I also want to rewrite it all for llvm backend, but I'm extremely busy right now...
-\
-\
-![image](https://user-images.githubusercontent.com/86890989/187889033-cefc38d6-a4a2-458e-8b0a-cc21c07e97b0.png)
+A DCS system built around 8-bit AVR micro controller.
+
+## Key Features
+- Multi master protocol loosely based on P-NET
+- Control units can be uploaded on the fly
+- Modify and see propagation of the signal on the running algorithms
+- Signal quality bit propagation
+- Each micro contoller is an independet unit which can send requests to other devices on the network
+- Logic can be tested on virtual controlles
+- Controllers supports integration with custom modules via I2C and memory addresses mapping
+
+## Dependencies on Windows
+- cygwin https://www.cygwin.com
+- make https://gnuwin32.sourceforge.net/packages/make.htm
+- perl https://strawberryperl.com
+- boost https://www.boost.org/
+- leveldb https://github.com/google/leveldb
+- scintilla https://www.scintilla.org/SciTEDownload.html
+- wtl https://wtl.sourceforge.io/
+- AVR toolchain https://www.microchip.com/en-us/tools-resources/develop/microchip-studio/gcc-compilers
+- avrdude https://www.nongnu.org/avrdude
+
+## Pictures
+
 <p align="center">
+  <b>How it looks</b>
+  <img src="https://user-images.githubusercontent.com/86890989/187889033-cefc38d6-a4a2-458e-8b0a-cc21c07e97b0.png">
   <b>Structural Diagram</b>
-  <br/><br/>
   <img src="https://user-images.githubusercontent.com/86890989/194228189-b999a2b3-aa35-41a7-9b53-975eb641f8f7.png">
 </p>
+
+## TODO
+- [ ] A proper compiler based on LLVM, maybe? With a support for ST language and quality bit propagation.
+- [ ] Probably need to remove bit values. It's hard to deal with.
+- [ ] Sort dependencies and load all algorithm at once.
+- [ ] Add more tests.
+- [ ] Add another more powerfull MC, maybe SBP based on Linux (probably a driver will be required for the protocol).
+- [ ] Add more FBD blocks (structured text block, etc...).
+- [ ] Add SFC language support
+
+## References
+1. [MAX3082 Transcievers [PDF]](https://www.analog.com/media/jp/technical-documentation/data-sheets/1522.pdf)
+2. [Detection of RS-485 signal loss [PDF]](https://www.ti.com/lit/an/slyt257/slyt257.pdf?ts=1746389303529&ref_url=https%253A%252F%252Fwww.google.com%252F)
+3. [P-NET [PDF]](https://www.p-net.org/old/download/mctext.pdf)
--- sim/src/medium.cpp
+++ sim/src/medium.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2021 nikitapnn1@gmail.com
-// This file is a part of npsystem (Distributed Control System) and covered by LICENSING file in the topmost directory
+// This file is a part of npsystem (Distributed Control System) and covered by
+// LICENSING file in the topmost directory
 
 #include "stdafx.h"
 #include <sim/medium.h>
@@ -8,89 +9,103 @@
 #include "intrisics.h"
 
 SIM_IMPORT_EXPORT
-Medium::Medium()
-	: controllers_count_(0) {
-	paused_.store(false);
-	running_.store(false);
-	controllers_.fill(nullptr);
+Medium::Medium() : controllers_count_(0)
+{
+  paused_.store(false);
+  running_.store(false);
+  controllers_.fill(nullptr);
 }
 
-void Medium::start() noexcept {
-	running_ = true;
-	medium_thread_ = std::thread(&Medium::MediumProc, this);
-	core_thread_ = std::thread(&Medium::CoreProc, this);
+void Medium::start() noexcept
+{
+  running_       = true;
+  medium_thread_ = std::thread(&Medium::MediumProc, this);
+  core_thread_   = std::thread(&Medium::CoreProc, this);
 }
 
-void Medium::stop() noexcept {
-	running_ = false;
-	medium_thread_.join();
-	core_thread_.join();
-}
-
-SIM_IMPORT_EXPORT
-void Medium::AddController(IController* controller) {
-	pause_execution([&]() {
-		controllers_[controllers_count_] = controller;
-		controllers_count_++;
-		controller->SetMedium(this);
-	});
-
-	if (running_ == false) start();
+void Medium::stop() noexcept
+{
+  running_ = false;
+  medium_thread_.join();
+  core_thread_.join();
 }
 
 SIM_IMPORT_EXPORT
-bool Medium::RemoveController(IController* controller) {
-	auto founded = std::find(controllers_.begin(), controllers_.end(), controller);
-	if (founded == controllers_.end()) return false;
-	pause_execution([&]() {
-		std::copy(std::next(founded), controllers_.end(), founded);
-		controllers_count_--;
-	});
-	
-	if (controllers_count_ == 0) stop();
+void Medium::AddController(
+  IController* controller)
+{
+  pause_execution([&]() {
+    controllers_[controllers_count_] = controller;
+    controllers_count_++;
+    controller->SetMedium(this);
+  });
 
-	return true;
+  if (running_ == false) start();
 }
 
-int Medium::MediumProc() noexcept {
-	namespace chrono = std::chrono;
-	auto tp2 = chrono::high_resolution_clock::now();
-	while (running_.load(std::memory_order_relaxed)) {
-		wait_if_paused();
-		auto tp1 = chrono::high_resolution_clock::now();
-		auto duration = chrono::duration_cast<chrono::nanoseconds>(tp1 - tp2);
-		tp2 = tp1;
+SIM_IMPORT_EXPORT
+bool Medium::RemoveController(
+  IController* controller)
+{
+  auto founded =
+    std::find(controllers_.begin(), controllers_.end(), controller);
+  if (founded == controllers_.end()) return false;
+  pause_execution([&]() {
+    std::copy(std::next(founded), controllers_.end(), founded);
+    controllers_count_--;
+  });
 
-		mstate_.run(duration.count());
+  if (controllers_count_ == 0) stop();
 
-		for (size_t ix = 0; ix < controllers_count_; ++ix) {
-			controllers_[ix]->ExecutePeripheral(duration.count());
-		}
-	}
-	return 0;
+  return true;
 }
 
-int Medium::CoreProc() noexcept {
-	try {
-		while (running_.load(std::memory_order_relaxed)) {
-			wait_if_paused();
-			if (!controllers_count_) continue;
-			auto tp0 = std::chrono::high_resolution_clock::now();
-			uint64_t duration = 0ull;
-			for (size_t ix = 0; ix < controllers_count_; ++ix) {
-				duration += controllers_[ix]->CoreStep();
-            }
-			duration = duration / controllers_count_;
-            std::chrono::high_resolution_clock::time_point tp1;
-			uint64_t real_duration;
-            do {
-				tp1 = std::chrono::high_resolution_clock::now();
-				real_duration = static_cast<uint64_t>(std::chrono::duration_cast<std::chrono::nanoseconds>(tp1 - tp0).count());
-			} while (real_duration < duration);
-		}
-	} catch (std::exception& ex) {
-		std::cerr << "AVR EXECUTION ERROR: " << ex.what() << '\n';
-		return -1;
-	}
-	return 0;
+int Medium::MediumProc() noexcept
+{
+  namespace chrono = std::chrono;
+  auto tp2         = chrono::high_resolution_clock::now();
+  while (running_.load(std::memory_order_relaxed)) {
+    wait_if_paused();
+    auto tp1      = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::nanoseconds>(tp1 - tp2);
+    tp2           = tp1;
+
+    mstate_.run(duration.count());
+
+    for (size_t ix = 0; ix < controllers_count_; ++ix) {
+      controllers_[ix]->ExecutePeripheral(duration.count());
+    }
+  }
+  return 0;
+}
+
+int Medium::CoreProc() noexcept
+{
+  try {
+    while (running_.load(std::memory_order_relaxed)) {
+      wait_if_paused();
+      if (!controllers_count_) continue;
+      // auto     tp0      = std::chrono::high_resolution_clock::now();
+      // uint64_t duration = 0ull;
+      for (size_t ix = 0; ix < controllers_count_; ++ix) {
+        // duration +=
+        controllers_[ix]->CoreStep();
+      }
+      /*
+      duration = duration / controllers_count_;
+      std::chrono::high_resolution_clock::time_point tp1;
+      uint64_t                                       real_duration;
+      do {
+        tp1           = std::chrono::high_resolution_clock::now();
+        real_duration = static_cast<uint64_t>(
+          std::chrono::duration_cast<std::chrono::nanoseconds>(tp1 - tp0)
+            .count());
+      } while (real_duration < duration);
+      */
+    }
+  } catch (std::exception& ex) {
+    std::cerr << "AVR EXECUTION ERROR: " << ex.what() << '\n';
+    return -1;
+  }
+  return 0;
 }
