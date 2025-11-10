0cff14b687a69c2767c6835ee5d35fe8230f7251
Nikita Pennie <nikitapnn1@gmail.com>
2025-11-11 00:32:56 +0300

    update copyright

--- nprpc/npnameserver/npnameserver.cpp
+++ nprpc/npnameserver/npnameserver.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 nikitapnn1@gmail.com
+// Copyright (c) 2021-2025, Nikita Pennie <nikitapnn1@gmail.com>
 // This file is a part of npsystem (Distributed Control System) and covered by LICENSING file in the topmost directory
 
 #include <iostream>
